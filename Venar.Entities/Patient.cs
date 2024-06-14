b02f38b1fe1b6c55c3c7ce1d0ffd24a34422aedf
noebelen <noebelengomez@gmail.com>
2024-06-13 23:53:36 -0300

    modificacion entidad patient

--- Venar.Entities/Patient.cs
+++ Venar.Entities/Patient.cs
@@ -6,27 +6,31 @@ using System.Threading.Tasks;
 
 namespace Venar.Entities
 {
-<<<<<<< Updated upstream
-    public class Patient 
-    {
-        public  Guid Id { get; set; } 
-        public string nombre  { get; set; }
-        public string apellido { get; set; }
-        public int dni { get; set; }
-        public string obraSocialPaciente { get; set; }
 
-
-        public void SomeMethod()
+        public class Patient
         {
-            // El código combinado o resuelto que deseas conservar
-        }
-        public Patient(string nombre, string apellido, int dni,string obraSocialPaciente)
-        {
-        this.nombre = nombre;
-            this.apellido = apellido;   
-            this.dni = dni;
-            this.obraSocialPaciente = obraSocialPaciente;
-        }
+            public Guid Id { get; set; }
+            public string name { get; set; }
+            public string lastName { get; set; }
+            public int dni { get; set; }
+            public string MedicalCoverage { get; set; }
+            public short gender { get; set; }
+            public string localidad { get; set; }
 
+            public Patient()
+            {
+            }
+
+            public Patient(Guid id, string name, string lastName, int dni, string medicalCoverage, short gender, string localidad)
+            {
+                Id = id;
+                this.name = name;
+                this.lastName = lastName;
+                this.dni = dni;
+                MedicalCoverage = medicalCoverage;
+                this.gender = gender;
+                this.localidad = localidad;
+            }
+        }
     }
-}
+
